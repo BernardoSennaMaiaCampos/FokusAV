3c3fc72ce49b6b2d4da307960b184e4899a15678
BernardoSennaMC <bernardosennamc@gmail.com>
2025-10-26 04:45:09 -0300

    add: StyleSheet

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import {Text, View, StyleSheet} from "react-native";
+
+export default function Index(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>Edit app/index.jsx to edit this screen.</Text>
+            </View>
+        );
+    }
+
+const styles = StyleSheet.create({
+    container: {
+       flex: 1,
+       justifyContent:"center",
+       alignItems:"center",
+       backgroundColor: "#021123"
+    },
+
+    text:{
+        color: "#fff"
+    },
+
+});
+
